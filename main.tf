b50a22d3f4b3b8c67e8c0507d22324f1826b6122
ytzhak-devops <107072401+ytzhak-devops@users.noreply.github.com>
2023-02-05 11:57:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,5 @@ provider "aws" {
 resource "aws_instance" "ytzhak_cloud_server2" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  count = 3
-
-lifecycle {
-  
-  ignore_changes = ["ami",]
-  }
-
-
+  count = 1
 }
